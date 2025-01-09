8aa4c6e60ed5bb11d1e3622c79b638dc0204a165
malcomcrab <malcomcrabesq@outlook.com>
2025-01-09 13:47:51 +0000

    Installed and configured eslint and prettier. Updated README.md with the assignments tasks.
    T
    bash: line 1: t: command not found

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+];
