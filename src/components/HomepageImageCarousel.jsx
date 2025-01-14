e62db2131630a518edd03da0e8e06816d99c0dc4
malcomcrab <malcomcrabesq@outlook.com>
2025-01-14 14:14:28 +0000

    Added carousel/slideshow element to the homepage displaying different photos in intervals

---
+++ src/components/HomepageImageCarousel.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react"
+import homepage1 from "../assets/images/homepage1.jpg"
+import homepage2 from "../assets/images/homepage2.jpg"
+import homepage3 from "../assets/images/homepage3.jpg"
+
+
+function HomepageImageCarousel() {
+
+    const imageArray = [
+        homepage1,
+        homepage2,
+        homepage3
+    ]
+
+    useEffect(() => {
+        const imageElement = document.querySelector('.carousel-image')
+        let i = 0
+        const key = setInterval(() => {
+            imageElement.src = imageArray[i]
+            i > 1 ? i = 0 : i++
+        }, 3000);
+        return () => {
+            clearInterval(key);
+        };
+    }, [])
+
+
+    return (
+        <div className="carousel-container">
+            <img className="carousel-image" width='100%' src={homepage3} />
+        </div>
+    )
+
+
+}
+
+export default HomepageImageCarousel;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
+import HomepageImageCarousel from "../components/HomepageImageCarousel";
 import InfoBar from "../components/InfoBar";
 
 function HomePage() {
   return (
     <div id="homepage-body">
       <Header />
+
+      <HomepageImageCarousel/>
 
       <InfoBar />
     </div>
